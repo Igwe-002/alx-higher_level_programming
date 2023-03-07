3ef550e1498d9a2ca655422725f6e3d78f32e1b3
Igwe-002 <miracleigwe48@hotmail.com>
2023-03-07 10:50:16 -0800

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
-for letter in range(97, 123):
-    if chr(letter) is not 'q' or chr(letter) is not 'e':
-        print("{}".format(chr(letter)), end="")
+for alpha_letters in range(97, 122):
+      if alpha_letters == ord(e) or alpha_letters == ord(q):
+         continue
+      print("{:c}".format(alpha_letters), end="")
